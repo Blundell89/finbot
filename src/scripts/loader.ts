ed53d29ae98bec07c4ff399f2a869abffaa389a0
Lee Blundell <leeblundell16@gmail.com>
2016-06-14 15:28:08 +0100

    Update loader

--- src/scripts/loader.ts
+++ src/scripts/loader.ts
@@ -5,9 +5,11 @@ import { IRobot } from 'hubot';
 import HelloScript from '../lib/hello';
 import BuildScript from '../lib/build';
 import DeployScript from '../lib/deploy';
+import GuidScript from '../lib/guid';
 
 module.exports = (robot: IRobot) => {
   HelloScript(robot);
   BuildScript(robot);
   DeployScript(robot);
+  GuidScript(robot);
 };
